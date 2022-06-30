f7caa7a5639ab4a29ddb9da5dbbd6bc629ec2a9c
Nikoloz Chitashvili <chitashvili.nikoloz@gmail.com>
2022-06-30 10:25:55 +0400

    Added 3 labels for random number representation

--- NikolozChitashvili(HW11)/NikolozChitashvili(HW11)/ViewController.swift
+++ NikolozChitashvili(HW11)/NikolozChitashvili(HW11)/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelFirst: UILabel!
+    @IBOutlet weak var labelSecond: UILabel!
+    @IBOutlet weak var labelThird: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateRandomValuesForLabels()
     }
 
 
+    // MARK: - Private section
+    private func updateRandomValuesForLabels() {
+        labelFirst.text  = self.getRandomInt()
+        labelSecond.text = self.getRandomInt()
+        labelThird.text  = self.getRandomInt()
+    }
+    
+    private func getRandomInt() -> String {
+        let lowValue = 0
+        let upperValue = 100
+        return String(Int.random(in: lowValue...upperValue))
+    }
 }
 
