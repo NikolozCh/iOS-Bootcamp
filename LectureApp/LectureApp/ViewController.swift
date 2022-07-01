9097c5ae16f73ad46756191b18acec197cdef049
Nikoloz Chitashvili <chitashvili.nikoloz@gmail.com>
2022-07-01 21:58:19 +0400

    Init commit

---
+++ LectureApp/LectureApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LectureApp
+//
+//  Created by Nikoloz Chitashvili on 01.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
