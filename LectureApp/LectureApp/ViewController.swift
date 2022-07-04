4330b1b53863a10cf531a527c069ec5602f50d30
Nikoloz Chitashvili <chitashvili.nikoloz@gmail.com>
2022-07-04 22:26:34 +0400

    Remove old changes

--- LectureApp/LectureApp/ViewController.swift
+++ LectureApp/LectureApp/ViewController.swift
@@ -9,33 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var ourLabel: UILabel!
-    let uiColor: [UIColor] = [.blue, .brown, .cyan, .red]
-    var appColor: UIColor?
-    var labelColor: UIColor?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-    @IBAction func changeAppBackground(_ sender: Any) {
-        appColor = uiColor.randomElement() ?? .white
-        if(appColor == labelColor) {
-            ourLabel.text = "Same color"
-        }
-        else {
-            ourLabel.text = "My text"
-        }
-        self.view.backgroundColor =  appColor
-    }
-    @IBAction func changeLabelBackground(_ sender: Any) {
-        labelColor = uiColor.randomElement() ?? .black
-        if(appColor == labelColor) {
-            ourLabel.text = "Same color"
-        }
-        else {
-            ourLabel.text = "My text"
-        }
-        ourLabel.backgroundColor = labelColor
     }
 }
