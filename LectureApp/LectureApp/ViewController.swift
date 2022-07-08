62d8ec58657ca684cb7f3c5f7247699aebcee76e
Nikoloz Chitashvili <chitashvili.nikoloz@gmail.com>
2022-07-08 22:41:29 +0400

    Original

--- LectureApp/LectureApp/ViewController.swift
+++ LectureApp/LectureApp/ViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lop: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        lop.layer.cornerRadius = 45
     }
 }
