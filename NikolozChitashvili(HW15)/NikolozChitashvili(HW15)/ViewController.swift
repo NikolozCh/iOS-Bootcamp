4af348f9c544b341e12580afe3cb3894e1665d6f
Nikoloz Chitashvili <chitashvili.nikoloz@gmail.com>
2022-07-12 12:08:27 +0400

    Init commit

---
+++ NikolozChitashvili(HW15)/NikolozChitashvili(HW15)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NikolozChitashvili(HW15)
+//
+//  Created by Nikoloz Chitashvili on 12.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
