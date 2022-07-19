e9091e6e50a8380aad45cb297231ba9275ad769d
Nikoloz Chitashvili <chitashvili.nikoloz@gmail.com>
2022-07-19 12:00:47 +0400

    First page finished partially

--- NikolozChitashvili(HW18)/NikolozChitashvili(HW18)/ViewController.swift
+++ NikolozChitashvili(HW18)/NikolozChitashvili(HW18)/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var blueView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setRoundedCorners()
     }
-
-
+    
+    @IBAction func redViewClicked(_ sender: Any) {
+        performSegue(withIdentifier: "nextPage", sender: self)
+    }
+    
+    @IBAction func blueViewClicked(_ sender: Any) {
+        performSegue(withIdentifier: "nextPage", sender: self)
+    }
+    
+    
+    private func setRoundedCorners() {
+        setRounded(view: redView)
+        setRounded(view: blueView)
+    }
+    
+    private func setRounded(view el: UIView) {
+        el.layer.cornerRadius = el.bounds.height / 2
+        el.alpha = 0.9
+    }
+    
 }
 
